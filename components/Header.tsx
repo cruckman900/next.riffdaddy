ff755b4aa2886019475ca1a8253cc1edc39b302b
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-10-02 05:14:30 -0400

    alt tag

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,8 +6,8 @@ export default function Header() {
         <header className="w-full bg-black text-white px-r py-3 flex items-center justify-between shadow-md">
             <Link href="/" className="flex items-center gap-3">
                 <Image
-                    src="/fanTABulous_Badge.png"
-                    alt="fanTABulous Logo"
+                    src="/NEXTRiff_Badge.png"
+                    alt="NEXTRiff Logo"
                     width={40}
                     height={40}
                     className="drop-shadow-[0_0_8px_#00ff80]"
