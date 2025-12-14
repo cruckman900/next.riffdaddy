248522973b5718b2efd662eb6e97a9b8b1b9e8f3
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-12-14 18:27:30 -0500

    define NoteIcon

---
+++ src/components/icons/NoteIcon.tsx
@@ -0,0 +1,9 @@
+import { IconBase, IconProps } from './IconBase';
+
+export function NoteIcon(props: IconProps) {
+    return (
+        <IconBase {...props}>
+            <path d="M9 4v10a3 3 0 1 0 2-2.83V6h5V4H9z" />
+        </IconBase>
+    );
+}
